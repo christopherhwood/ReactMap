ca5e6785c3d819386ea1ca31ca548d80954aa5b6
Chris Wood <chris.wood@daydaycook.com>
2018-02-11 16:05:08 -0800

    cleanup

--- app/actions/searchActions.js
+++ app/actions/searchActions.js
@@ -1,10 +1,5 @@
 import * as types from './types';
 
-export const setQuery = (query) => ({
-  type: types.SET_QUERY,
-  query
-});
-
 export const receiveAutocomplete = (response) => ({
   type: types.RECEIVE_AUTOCOMPLETE,
   response
